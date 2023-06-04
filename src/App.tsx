9c0639e2e4f0f82d03adae99139df7a7135179ca
Oleksij Rozhkov <oleksij.rozhkov@flip.shop>
2023-06-04 14:31:40 +0300

    Add CircleCI config

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import { Routes,Route  } from "react-router-dom";
-import BoardPage from "./pages/board/BoardPage";
-import Login from './pages/login/Login'
-import Registration from "./pages/registration/Registration";
-import BoardsPage from "./pages/boards/BoardsPage";
+import { Routes, Route } from 'react-router-dom';
+import BoardPage from './pages/board/BoardPage';
+import Login from './pages/login/Login';
+import Registration from './pages/registration/Registration';
+import BoardsPage from './pages/boards/BoardsPage';
 
 function App() {
   return (
-    <Routes >
-      <Route path="/boards" element={<BoardsPage />}/>
-      <Route path="/boards/:id" element={<BoardPage />}/>
-      <Route path="/login" element={<Login />}/>
-      <Route path="/registration" element={<Registration />}/>
-      <Route path="/*" element={<BoardsPage />}/>
+    <Routes>
+      <Route path="/boards" element={<BoardsPage />} />
+      <Route path="/boards/:id" element={<BoardPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registration" element={<Registration />} />
+      <Route path="/*" element={<BoardsPage />} />
     </Routes>
-  )
-  
-  ;
+  );
 }
 
 export default App;
